609d571182da96f57b7793a96f6caa7289bc1e5d
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-11 00:58:33 +0000

    pair programmed wordsearch assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,52 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//pair programmed with Elliott  and Calvin
+
+const transpose = function(matrix) {
+    // Put your solution here
+    let newArray = [];
+    
+    // for each column (values in nested array at [0]), add an array to newArray
+    for (let x = 0; x < matrix[0].length; x++) {
+      newArray.push([]);
+
+      // add each element by column into each new array (all 1st elements to array at[0], 2nd elements to array at [1], ...)
+      for (let y = 0; y < matrix.length; y++) {
+        newArray[x].push(matrix[y][x]);
+          }
+    return newArray;
+  };
+
+  // Do not edit this function.
+const printMatrix = (matrix) => {
+    for (const row of matrix) {
+        for (const el of row) {
+            process.stdout.write(el + " ");
+        }
+        process.stdout.write('\n')
     }
 }
 
+const wordSearch = (letters, word) => { 
+    let array = [];
+
+    const horizontalJoin = letters.map(ls => ls.join(''))
+        for (l of horizontalJoin) {
+            if (l.includes(word)) return true        
+        } 
+
+        const vertical = transpose(letters);
+    
+
+        const verticalJoin = vertical.map(ls => ls.join(''))
+        for (l of verticalJoin) {
+            if (l.includes(word)) return true
+            
+        }
+    
+    
+    return false
+}
+
 module.exports = wordSearch
+
+
+ 
